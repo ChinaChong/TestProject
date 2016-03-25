920b7dd0d6830819c3bdfeabfe7f311427aa7731
ChinaChong <chinachong1943@yahoo.com>
2016-03-25 14:56:49 +0800

    提交

---
+++ GitTest01/GitTest01/ViewController.m
@@ -0,0 +1,35 @@
+//
+//  ViewController.m
+//  GitTest01
+//
+//  Created by ChinaChong on 16/3/25.
+//  Copyright © 2016年 ChinaChong. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    NSLog(@"我是皇帝");
+    
+    NSLog(@"万古长青,分支加入");
+    
+    NSLog(@"一统江湖");
+    
+    NSLog(@"孝显赫哈哈哈");
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
